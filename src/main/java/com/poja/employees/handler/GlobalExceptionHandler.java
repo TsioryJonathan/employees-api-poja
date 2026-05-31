6bb3483d73bb06929a03d04be6b24568578c4527
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-05-31 20:34:53 +0300

    feat: return an error response when employee not found

---
+++ src/main/java/com/poja/employees/handler/GlobalExceptionHandler.java
@@ -0,0 +1,19 @@
+package com.poja.employees.handler;
+import com.poja.employees.model.exception.NotFoundException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<Map<String, String>> handleNotFoundException(NotFoundException e) {
+        Map<String, String> map = new HashMap<>();
+        map.put("error", e.getMessage());
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(map);
+    }
+}
