0f1a17b2f34555dc06f7baf6c4e2eb2d65efda0d
Tsiory Jonathan <rakotoarisoajonathan0@gmail.com>
2026-06-01 12:49:02 +0300

    chore: format code

--- src/main/java/com/poja/employees/endpoint/WebConfig.java
+++ src/main/java/com/poja/employees/endpoint/WebConfig.java
@@ -15,7 +15,7 @@ public class WebConfig {
       public void addCorsMappings(CorsRegistry registry) {
         registry
             .addMapping("/**")
-            .allowedOrigins("http://localhost:5173","https://employees-admin-puce.vercel.app")
+            .allowedOrigins("http://localhost:5173", "https://employees-admin-puce.vercel.app")
             .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
             .allowedHeaders("*")
             .allowCredentials(true);
